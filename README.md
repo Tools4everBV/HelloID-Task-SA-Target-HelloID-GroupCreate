f33d97076af86b06b0fec7917b15acf384cd954a
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-11-20 19:00:44 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,54 +1,45 @@
-# HelloID-Task-SA-Target-HelloID-Template
-Template repository for HelloID Service Automation Task
-
-<a href="https://github.com/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template/network/members"><img src="https://img.shields.io/github/forks/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template" alt="Forks Badge"/></a>
-<a href="https://github.com/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template/pulls"><img src="https://img.shields.io/github/issues-pr/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template" alt="Pull Requests Badge"/></a>
-<a href="https://github.com/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template/issues"><img src="https://img.shields.io/github/issues/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template" alt="Issues Badge"/></a>
-<a href="https://github.com/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template/graphs/contributors"><img alt="GitHub contributors" src="https://img.shields.io/github/contributors/Tools4everBV/HelloID-Task-SA-Target-HelloID-Template?color=2b9348"></a>
-
-| :warning: Warning                                                                             |
-| :-------------------------------------------------------------------------------------------- |
-| This is a template repository. Please make sure you to change the files and code accordingly. |
+# HelloID-Task-SA-Target-ActiveDirectory-GroupCreate
 
 ## Prerequisites
-The requirements to run this connector, such as, an App Registration, to be run on-premises, run with concurrent sessions set to a max. of 1, etc.
-An example for a HelloID "Create User" task is given below:
 
 - [ ] HelloID API key and secret
 - [ ] Pre-defined variables: `portalBaseUrl`, `portalApiKey` and `portalApiSecret` created in your HelloID portal.
 
 ## Description
-A description of the task and the possibilities, such as, the formobject that is used as input and what what actions are performed.
-An example for a HelloID "Create User" task is given below:
 
-This code snippet will create a new user within HelloID and executes the following tasks:
+This code snippet will create a new group within HelloID and executes the following tasks:
 
-1. Define a hash table `$formObject`. The keys of the hash table represent the properties necessary to create a new user within `HelloID`, while the values represent the values entered in the form.
+1. Define a hash table `$formObject`. The keys of the hash table represent the properties necessary to create a new group within `HelloID`, while the values represent the values entered in the form.
 
 > To view an example of the form output, please refer to the JSON code pasted below.
 
 ```json
 {
-    "userName": "john.doe",
-    "firstName": "john",
-    "lastName": "doe",
-    "contactEmail": "johndoe@enyoi.org",
+    "name": "John Doe's Group",
     "isEnabled": "true",
-    "password": "P@ssw0rd1!",
-    "mustChangePassword": "true",
-    "employeeid": "12345678",
-    "title": "tester",
-    "department": "test",
-    "phonenumber": "0612345678",
-    "manager": {
-        "UserGUID": "8a6b62fc-dca3-4890-bacd-3db76c5e9152"
-    }
+    "isDefault": "false",
+    "userGuids": [
+        "953d9fdb-e303-4c9a-8e97-7cae5c8c512b",
+        "a74295b1-75a4-403c-86b8-021494c7fb72"
+    ],
+    "userNames": [
+        "john.doe@enyoi.org",
+        "jane.doe@enyoi.org"
+    ],
+    "applicationNames": [
+        "Generic SAML Application",
+        "Generic OIDC Application"
+    ],
+    "managedByUserGuid": "b27a9d33-d660-4f48-a338-e5013a688991",
+    "applicationGUIDs": [
+        "6937b1f8-9294-45b7-a0ed-ccba3bcff325"
+    ]
 }
 ```
 
 > :exclamation: It is important to note that the names of your form fields might differ. Ensure that the `$formObject` hash table is appropriately adjusted to match your form fields.
-> [See the HelloID API Docs page](https://apidocs.helloid.com/docs/helloid/7d9592b2cfeed-add-a-user)
+> [See the HelloID API Docs page](https://apidocs.helloid.com/docs/helloid/0b84c01989115-add-a-group)
 
 2. Creates authorization headers using the provided API key and secret.
 
-3. Create a new user using the: `Invoke-RestMethod` cmdlet. The hash table called: `$formObject` is passed to the body of the: `Invoke-RestMethod` cmdlet as a JSON object.
+3. Create a new group using the: `Invoke-RestMethod` cmdlet. The hash table called: `$formObject` is passed to the body of the: `Invoke-RestMethod` cmdlet as a JSON object.
--- action.ps1
+++ action.ps1
@@ -1,29 +1,30 @@
-# HelloID-Task-SA-Target-<System>-<Description of action>
+# HelloID-Task-SA-Target-HelloID-GroupCreate
 ###########################################################
 # Form mapping
 $formObject = @{
-    userName           = $form.userName
-    firstName          = $form.firstName
-    lastName           = $form.lastName
-    contactEmail       = $form.contactEmail
-    isEnabled          = [bool]$form.isEnabled
-    password           = $form.password
-    mustChangePassword = [bool]$form.mustChangePassword
+    name              = $form.name
+    isEnabled         = [bool]$form.isEnabled
+    isDefault         = [bool]$form.isDefault
+    userNames         = $form.userNames
+    userGuids         = $form.userGuids
+    managedByUserGuid = $form.managedByUserGuid
+    applicationNames  = $form.applicationNames
+    applicationGUIDs  = $form.applicationGUIDs
 }
 
 try {
-    Write-Information "Executing <System> action: [CreateAccount] for: [$($formObject.userName)]"
+    Write-Information "Executing HelloID action: [CreateResource] for Group: [$($formObject.name)]"
     Write-Verbose "Creating authorization headers"
-    # Create authorization headers with <System> API key
+    # Create authorization headers with HelloID API key
     $pair = "${portalApiKey}:${portalApiSecret}"
     $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
     $base64 = [System.Convert]::ToBase64String($bytes)
     $key = "Basic $base64"
     $headers = @{"authorization" = $Key }
 
-    Write-Verbose "Creating <System>Account for: [$($formObject.userName)]"
+    Write-Verbose "Creating HelloIDAccount for Group: [$($formObject.name)]"
     $splatCreateUserParams = @{
-        Uri         = "$($portalBaseUrl)/api/v1/users"
+        Uri         = "$($portalBaseUrl)/api/v1/groups"
         Method      = "POST"
         Body        = ([System.Text.Encoding]::UTF8.GetBytes(($formObject | ConvertTo-Json -Depth 10)))
         Verbose     = $false
@@ -33,32 +34,32 @@ try {
     $response = Invoke-RestMethod @splatCreateUserParams
 
     $auditLog = @{
-        Action            = "CreateAccount"
-        System            = "<System>"
+        Action            = "CreateResource"
+        System            = "HelloID"
         TargetIdentifier  = [String]$response.userGUID
         TargetDisplayName = [String]$response.userName
-        Message           = "<System> action: [CreateAccount] for: [$($formObject.userName)] executed successfully"
+        Message           = "HelloID action: [CreateResource] for Group: [$($formObject.name)] executed successfully"
         IsError           = $false
     }
     Write-Information -Tags "Audit" -MessageData $auditLog
 
-    Write-Information "<System> action: [CreateAccount] for: [$($formObject.userName)] executed successfully"
+    Write-Information "HelloID action: [CreateResource] for Group: [$($formObject.name)] executed successfully"
 }
 catch {
     $ex = $_
     $auditLog = @{
-        Action            = "CreateAccount"
-        System            = "<System>"
+        Action            = "CreateResource"
+        System            = "HelloID"
         TargetIdentifier  = ""
         TargetDisplayName = [String]$formObject.userName
-        Message           = "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.Exception.Message)"
+        Message           = "Could not execute HelloID action: [CreateResource] for Group: [$($formObject.name)], error: $($ex.Exception.Message)"
         IsError           = $true
     }
     if ($($ex.Exception.GetType().FullName -eq "Microsoft.PowerShell.Commands.HttpResponseException")) {
-        $auditLog.Message = "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)]"
-        Write-Error "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.ErrorDetails)"
+        $auditLog.Message = "Could not execute HelloID action: [CreateResource] for Group: [$($formObject.name)]"
+        Write-Error "Could not execute HelloID action: [CreateResource] for Group: [$($formObject.name)], error: $($ex.ErrorDetails)"
     }
     Write-Information -Tags "Audit" -MessageData $auditLog
-    Write-Error "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.Exception.Message)"
+    Write-Error "Could not execute HelloID action: [CreateResource] for Group: [$($formObject.name)], error: $($ex.Exception.Message)"
 }
 ###########################################################
